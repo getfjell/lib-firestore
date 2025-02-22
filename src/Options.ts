444f8e3b43b97acf80aa27262a76f7007f6c998e
Fjell Project <info@getfjell.com>
2025-02-22 15:12:51 -0800

    Fixed a typescript error

--- src/Options.ts
+++ src/Options.ts
@@ -9,7 +9,8 @@ L2 extends string = never,
 L3 extends string = never,
 L4 extends string = never,
 L5 extends string = never
->(libOptions?: Library.Options<V, S, L1, L2, L3, L4, L5>) => {
+>(libOptions?: Library.Options<V, S, L1, L2, L3, L4, L5>):
+  Library.Options<V, S, L1, L2, L3, L4, L5> => {
   const options = Library.createOptions(libOptions);
   return {
     ...options
