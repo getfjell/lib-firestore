9030645441f170ec5fa12a3591431fadf7883503
Tim O'Brien <tobrien@discursive.com>
2025-03-11 08:24:50 -0700

    Exposing the DocProcessor methods (temporarily) there is a component that needs to be able to process responses from Firestore

--- src/index.ts
+++ src/index.ts
@@ -5,3 +5,8 @@ export * from './Options';
 export * from './Operations';
 export * as Contained from './contained';
 export * as Primary from './primary';
+
+// TODO: Exporting this should be temporary.  client applications
+// are subscribing to updates from firestore, and because of this they need to be able to process the dates.
+// Once we have a proper client SDK, we should remove this.
+export * from './DocProcessor';
